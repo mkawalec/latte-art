382120ea26822d5edb5db01f30a230271094e6fd
Jakub Z <jakzale@gmail.com>
2014-09-01 22:39:20 +0100

    Add CoffeeScript preprocessor for Karma

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '"source/**/*.coffee"',
-      '"tests/**/*_test.coffee"'
+      'source/**/*.coffee',
+      'tests/**/*_test.coffee'
     ],
 
 
@@ -28,8 +28,20 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        '**/*.coffee': ['coffee']
     },
 
+    coffeePreprocessor: {
+      // options passed to the coffee compiler
+      options: {
+        bare: true,
+        sourceMap: true
+      },
+      // transforming the filenames
+      transformPath: function(path) {
+        return path.replace(/\.coffee$/, '.js');
+      }
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
